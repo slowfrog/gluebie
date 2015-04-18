03d4f69293b9f2dffc4ac6cee8fe15676717cf0d
SlowFrog <laurentvaucher@gmail.com>
2015-04-18 20:07:41 +0200

    Objects and keyboard handling.

--- main.js
+++ main.js
@@ -17,45 +17,74 @@ gameState.preload = function() {
 };
 
 gameState.create = function() {
-  var l = Level.create(10, 10);
+  this.level = Level.create(10, 10);
   for (var i = 0; i < 10; ++i) {
-    l.setBoard(i, 0, Level.WALL);
-    l.setBoard(0, i, Level.WALL);
-    l.setBoard(i, 9, Level.WALL);
-    l.setBoard(9, i, Level.WALL);
+    this.level.setBoard(i, 0, Level.WALL);
+    this.level.setBoard(0, i, Level.WALL);
+    this.level.setBoard(i, 9, Level.WALL);
+    this.level.setBoard(9, i, Level.WALL);
   }
-  l.setBoard(0, 4, Level.START);
-  l.setBoard(9, 5, Level.EXIT);
+  this.level.setBoard(0, 4, Level.START);
+  this.level.setBoard(9, 5, Level.EXIT);
 
-  l.getContent(3, 3).push(Item.create(Item.GLUE_TUBE, 5));
-  l.getContent(4, 4).push(Item.create(Item.BAG_PILE, 2));
-  l.getContent(5, 5).push(Item.create(Item.GLUE_STAIN));
+  this.level.addEntity(Item.create(3, 3, Item.GLUE_TUBE, 5));
+  this.level.addEntity(Item.create(4, 4, Item.BAG_PILE, 2));
+  this.level.addEntity(Item.create(5, 5, Item.GLUE_STAIN));
+  this.boy = Boy.create(6, 8);
+  this.level.addEntity(this.boy);
   
   // Render board
   this.board = game.add.group(undefined, "board", true);
-  for (var i = 0; i < l.getWidth(); ++i) {
-    for (var j = 0; j < l.getHeight(); ++j) {
-      game.add.sprite(10 + 32 * i, 10 + 32 * j, "tiles", l.getBoard(i, j), this.board);
+  for (var i = 0; i < this.level.getWidth(); ++i) {
+    for (var j = 0; j < this.level.getHeight(); ++j) {
+      game.add.sprite(10 + 32 * i, 10 + 32 * j, "tiles",
+                      BOARD_SPRITE[this.level.getBoard(i, j)], this.board);
     }
   }
 
+  this.arrows = game.input.keyboard.createCursorKeys();
+  console.log("HERE", this.arrows);
+
+  // Time
+  this.timer = new Phaser.Timer();
+  this.last = this.timer.ms;
+};
+
+gameState.update = function() {
+  // Check input
+  var d = 15; //this.timer.ms - this.last;
+  this.last = this.timer.ms;
+  if (this.arrows.down.downDuration(d)) {
+    this.boy.moveBy(0, 1);
+  } else if (this.arrows.right.downDuration(d)) {
+    this.boy.moveBy(1, 0);
+  } else if (this.arrows.up.downDuration(d)) {
+    this.boy.moveBy(0, -1);
+  } else if (this.arrows.left.downDuration(d)) {
+    this.boy.moveBy(-1, 0);
+  }
+  
   // Render contents
+  if (this.objects) {
+    this.objects.destroy();
+  }
   this.objects = game.add.group(undefined, "objects", true);
-  for (var i = 0; i < l.getWidth(); ++i) {
-    for (var j = 0; j < l.getHeight(); ++j) {
-      var content = l.getContent(i, j);
+  for (var i = 0; i < this.level.getWidth(); ++i) {
+    for (var j = 0; j < this.level.getHeight(); ++j) {
+      var content = this.level.getContent(i, j);
       for (var k = 0; k < content.length; ++k) {
         var obj = content[k];
         if (obj instanceof Item) {
-          console.log("XXX ", obj);
-          game.add.sprite(10 + 32 * i, 10 + 32 * j, "tiles", obj.getType() + 3, this.objects);
+          game.add.sprite(10 + 32 * i, 10 + 32 * j, "tiles",
+                          ITEM_SPRITE[obj.getType()], this.objects);
+        } else if (obj instanceof Boy) {
+          game.add.sprite(10 + 32 * i, 10 + 32 * j, "tiles",
+                          BOY_SPRITE, this.objects);
         }
       }
     }
   }
-};
-
-gameState.update = function() {
+  
 };
 
 // Wrap it up
--- model.js
+++ model.js
@@ -1,20 +1,158 @@
 "use strict";
 
+// Event
+var Event = function() {};
+
+Event.create = function(type) {
+  return new Event()._init_(type);
+};
+
+Event.prototype._init_ = function(type) {
+  this.type = type;
+  return this;
+};
+
+Event.prototype.getType = function() {
+  return this.type;
+};
+
+Event.prototype.setSource = function(s) {
+  this.source = s;
+};
+
+Event.prototype.getSource = function() {
+  return this.source;
+};
+
+// Observable
+var Observable = function() {};
+
+Observable.create = function() {
+  var o = new Observable();
+  o._init_();
+  return o;
+};
+
+Observable.prototype._init_ = function() {
+  this.observers = [];
+};
+
+Observable.prototype.addObserver = function(o) {
+  for (var i = 0; i < this.observers.length; ++i) {
+    if (this.observers[i] == o) {
+      return;
+    }
+  }
+  this.observers.push(o);
+};
+
+Observable.prototype.removeObserver = function(o) {
+  for (var i = 0; i < this.observers.length; ++i) {
+    if (this.observers[i] == o) {
+      this.observers = this.observers.splice(i, 1);
+      return;
+    }
+  }
+};
+
+Observable.prototype.notify = function(e) {
+  e.setSource(this);
+  for (var i = 0; i < this.observers.length; ++i) {
+    this.observers[i].notify(e);
+  }
+};
+
+
+// Entity
+var Entity = function() {};
+Entity.prototype = new Observable();
+
+Entity.MOVED = new Object();
+  
+Entity.create = function(x, y) {
+  return new Entity()._init_(x, y);
+};
+
+Entity.prototype._init_ = function(x, y) {
+  Observable.prototype._init_.call(this);
+  this.x = x;
+  this.y = y;
+  return this;
+};
+
+Entity.prototype.getX = function() {
+  return this.x;
+};
+
+Entity.prototype.getY = function() {
+  return this.y;
+};
+
+Entity.prototype.moveBy = function(dx, dy) {
+  this.moveTo(this.x + dx, this.y + dy);
+};
+
+Entity.prototype.moveTo = function(x, y) {
+  if (x != this.x || y != this.y) {
+    var prevX = this.x;
+    var prevY = this.y;
+    this.x = x;
+    this.y = y;
+    // norify move
+    this.notify(MoveEvent.create(prevX, prevY, this.x, this.y));
+  }
+};
+
+// MoveEvent
+var MoveEvent = function() {};
+MoveEvent.prototype = new Event();
+
+MoveEvent.create = function(x0, y0, x1, y1) {
+  return new MoveEvent()._init_(x0, y0, x1, y1);
+};
+
+MoveEvent.prototype._init_ = function(x0, y0, x1, y1) {
+  Event.prototype._init_.call(this, Entity.MOVED);
+  this.x0 = x0;
+  this.y0 = y0;
+  this.x1 = x1;
+  this.y1 = y1;
+  return this;
+};
+
+MoveEvent.prototype.getX0 = function() {
+  return this.x0;
+};
+
+MoveEvent.prototype.getY0 = function() {
+  return this.y0;
+};
+
+MoveEvent.prototype.getX1 = function() {
+  return this.x1;
+};
+
+MoveEvent.prototype.getY1 = function() {
+  return this.y1;
+};
+
+// Items
 var Item = function() {};
+Item.prototype = new Entity();
 
 Item.GLUE_TUBE = 1;
 Item.BAG_PILE = 2;
 Item.GLUE_STAIN = 3;
 
-Item.create = function(type, opt_value) {
-  var i = new Item();
-  i._init_(type, opt_value);
-  return i;
+Item.create = function(x, y, type, opt_value) {
+  return new Item()._init_(x, y, type, opt_value);
 };
 
-Item.prototype._init_ = function(type, opt_value) {
+Item.prototype._init_ = function(x, y, type, opt_value) {
+  Entity.prototype._init_.call(this, x, y);
   this.type = type;
   this.value = opt_value || 0;
+  return this;
 };
 
 Item.prototype.getType = function() {
@@ -23,6 +161,18 @@ Item.prototype.getType = function() {
 
 Item.prototype.getValue = function() {
   return this.value;
+};
+
+// Boy
+var Boy = function() {};
+Boy.prototype = new Entity();
+
+Boy.create = function(x, y) {
+  return new Boy()._init_(x, y);
+};
+
+Boy.prototype._init_ = function(x, y) {
+  return Entity.prototype._init_.call(this, x, y);
 };
 
 // Level contents
@@ -74,4 +224,23 @@ Level.prototype.setBoard = function(x, y, v) {
 
 Level.prototype.getContent = function(x, y) {
   return this.content[y][x];
+};
+
+Level.prototype.addEntity = function(e) {
+  this.getContent(e.getX(), e.getY()).push(e);
+  e.addObserver(this);
+};
+
+Level.prototype.notify = function(e) {
+  if (e.getType() === Entity.MOVED) {
+    var prevContent = this.getContent(e.getX0(), e.getY0());
+    for (var i = 0; i < prevContent.length; ++i) {
+      if (prevContent[i] === e.getSource()) {
+        prevContent.splice(i, 1);
+        this.getContent(e.getX1(), e.getY1()).push(e.getSource());
+        return;
+      }
+    }
+    console.log("Entity move not processed", e);
+  }
 };
